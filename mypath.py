89bc387996d0232d281dc8aeeb52226e2ed4d779
JimReno <752434996@qq.com>
2019-03-20 15:13:03 +0800

    add own data set path

--- mypath.py
+++ mypath.py
@@ -3,11 +3,18 @@ class Path(object):
     def db_dir(database):
         if database == 'ucf101':
             # folder that contains class labels
-            root_dir = '/Path/to/UCF-101'
+            root_dir = '/data/raw/video'
 
             # Save preprocess data into output_dir
-            output_dir = '/path/to/VAR/ucf101'
+            output_dir = '/data/preprocess/video'
 
+            return root_dir, output_dir
+        elif database == 'video_cls':
+            # folder that contains class labels
+            root_dir = '/home/nd/workspace/zjc/pytorch/pytorch-video-recognition/data/video_cls'
+
+            # Save preprocess data into output_dir
+            output_dir = '/home/nd/workspace/zjc/pytorch/pytorch-video-recognition/data/preprocess/video_cls/'
             return root_dir, output_dir
         elif database == 'hmdb51':
             # folder that contains class labels
@@ -22,4 +29,4 @@ class Path(object):
 
     @staticmethod
     def model_dir():
-        return '/path/to/Models/c3d-pretrained.pth'
+        return '/home/nd/workspace/zjc/pytorch/pytorch-video-recognition/model/c3d-pretrained.pth'
